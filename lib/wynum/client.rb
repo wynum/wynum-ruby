29171ddb1a609ea570318ed44d271731c39c8f70
Suraj Patil <surajp815@gmail.com>
2019-06-26 12:05:08 +0530

    changed ayload format according to new API changes

--- lib/wynum/client.rb
+++ lib/wynum/client.rb
@@ -9,8 +9,8 @@ class Client
   def initialize(secret, token)
     @secret = secret
     @token = token
-    @data_url = "https://api.wynum.com/data/#{@token}"
-    @schema_url = "https://api.wynum.com/component/#{@token}"
+    @data_url = "https://api.wynum.com/data/#{@token}?secret_key=#{@secret}"
+    @schema_url = "https://api.wynum.com/component/#{@token}?secret_key=#{@secret}"
   end
 
   def get_data
@@ -27,12 +27,34 @@ class Client
   end
 
   def post_data(data)
-    resposne = RestClient.post(@data_url, data.to_json, {content_type: :json, accept: :json})
+    if has_file data
+      form_data = prepare_form_data data
+      resposne = RestClient.post(@data_url, form_data)
+    else
+      resposne = RestClient.post(@data_url, data.to_json, {content_type: :json, accept: :json})
+    end
     return JSON.parse resposne.body
   end
 
   def update_data(data)
-    resposne = RestClient.put(@data_url, data.to_json, {content_type: :json, accept: :json})
+    if has_file data
+      form_data = prepare_form_data data
+      resposne = RestClient.put(@data_url, form_data)
+    else
+      resposne = RestClient.put(@data_url, data.to_json, {content_type: :json, accept: :json})
+    end
     return JSON.parse resposne.body
+  end
+
+  def has_file(data)
+    return data.any? { |key, val| val.class == File }
+  end
+
+  def prepare_form_data(data)
+    form_data = data.select { |key, val| val.class == File }
+    other_data = data.select { |key, val| val.class != File }
+    form_data[:inputdata] = other_data.to_json
+    form_data[:multipart] = true
+    return form_data
   end
 end
