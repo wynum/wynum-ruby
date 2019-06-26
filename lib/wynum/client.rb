9354f5da579a8c8b0ce161c43ef752516ee08387
Suraj Patil <surajp815@gmail.com>
2019-06-26 13:48:30 +0530

    implemented get_data

--- lib/wynum/client.rb
+++ lib/wynum/client.rb
@@ -13,8 +13,10 @@ class Client
     @schema_url = "https://api.wynum.com/component/#{@token}?secret_key=#{@secret}"
   end
 
-  def get_data
-    raise NotImplementedError
+  def get_data(params = {})
+    params = validate_and_parse_args params
+    resposne = RestClient.get @data_url, {params: params}
+    return JSON.parse resposne.body
   end
 
   def get_schema
@@ -56,5 +58,31 @@ class Client
     form_data[:inputdata] = other_data.to_json
     form_data[:multipart] = true
     return form_data
+  end
+
+  def validate_and_parse_args(args)
+    for key, val in args
+      case key
+      when :limit
+        if !val.is_a? Integer
+          raise ArgumentError.new "limit must be a non-negative integer"
+        end
+      when :ids
+        if !val.kind_of? Array
+          raise ArgumentError.new "ids must be an array of string values"
+        end
+        args[key] = val.join ","
+      when :order_by
+        if !["asc", "desc"].include? val
+          raise ArgumentError.new "order_by must be one 'asc' or 'desc'"
+        end
+        args[key] = val.upcase
+      when :start, :to
+        if !val.is_a? Integer
+          raise ArgumentError.new "#{key} must be a non-negative integer"
+        end
+      end
+    end
+    return args
   end
 end
